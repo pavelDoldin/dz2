e65bd3aa32862f1888ce324dc7a9c21e148e0aa3
pavelDoldin <doldin86@icloud.com>
2022-11-29 21:37:10 +0500

    z3

---
+++ z3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите цифру обозначающую день недели: ");
+int n = Convert.ToInt32(Console.ReadLine()); // Ввод числа
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись:(!\nВведите цифру обозначающую день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+     if    (n == 1)
+    Console.Write("no");
+else if (n == 2)
+    Console.Write("no");
+else if (n == 3)
+    Console.Write("no");
+else if (n == 4)
+    Console.Write("no");
+else if (n == 5)
+    Console.Write("no");
+else if (n == 6)
+    Console.Write("yes");
+else 
+    Console.Write("yes");
