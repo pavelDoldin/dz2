7c3024491b6eb3fc00c61cac3bd5c3ab7e8d265d
pavelDoldin <doldin86@icloud.com>
2022-11-29 20:45:59 +0500

    z2

--- z2/Program.cs
+++ z2/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.Write("нет третьей цифры");
+else
+{ 
+    while (n >=100)
+        n = n / 10;
+    Console.WriteLine(n % 10);
+}
