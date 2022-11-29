e5eac6635690a84636bf5a1d9cb6882dfda4ed58
pavelDoldin <doldin86@icloud.com>
2022-11-29 19:18:07 +0500

    z1

--- z1/Program.cs
+++ z1/Program.cs
@@ -1,7 +1,5 @@
 ﻿Console.Clear();
 Console.Write("Введите трёхзначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int n1 = n / 100;
 int n2 = n / 10;
-int n3 = n % 10;
     Console.Write(n2 % 10);
