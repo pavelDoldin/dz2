26fc7d3dac2c01bafb4cf70dda0de7b3bc267fde
pavelDoldin <doldin86@icloud.com>
2022-12-08 13:56:28 +0500

    dop3

---
+++ dz3_dop/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите количество кустов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] arrayFirst = new int [n];
+int resultSumma = 0;
+
+for (int i = 0; i < arrayFirst.Length; i++)
+    arrayFirst  [i] = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < arrayFirst.Length -1; i++)
+{
+    if (arrayFirst[i - 1] + arrayFirst[i] + arrayFirst[i + 1] > resultSumma)
+    resultSumma = arrayFirst[i - 1] + arrayFirst[i] + arrayFirst[i + 1];
+}
+if (arrayFirst[0] + arrayFirst[1] + arrayFirst[n -1] > resultSumma)
+    resultSumma = arrayFirst[0] + arrayFirst[1] + arrayFirst[n - 1];
+
+if (arrayFirst[0] + arrayFirst[n - 1] + arrayFirst [n - 2] > resultSumma)
+    resultSumma = arrayFirst[0] + arrayFirst[n - 1] + arrayFirst[n - 2];
+
+Console.WriteLine(resultSumma);
+
+
+
--- dz3_task19/Program.cs
+++ dz3_task19/Program.cs
@@ -2,7 +2,19 @@
 // Напишите программу, которая принемает на вход пятизначное число и проверяет его,
 // является ли оно палиндромом.
 
-
+Console.Clear();
+Console.Write("Введите любое число: "); //Нахождения палиндропа любого числа
+int n = Convert.ToInt32(Console.ReadLine());
+int result = n;
+int newNumber = 0, i = Convert.ToString(n).Length -1;
+while (i >= 0)
+{
+    newNumber = newNumber + (n % 10 * Convert.ToInt32(Math.Pow(10, i)));
+    n = n / 10;
+    i--;
+}
+Console.WriteLine(newNumber == result);
+/*
 // Чесно списал
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
@@ -21,3 +33,4 @@ if (n1 == n5 && n2 == n4)
     Console.WriteLine("yes");
 else 
     Console.WriteLine("no");
+*/
