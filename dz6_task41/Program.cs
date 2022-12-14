04e77f0169635f835baa9a5ec9079159e3df19b3
pavelDoldin <doldin86@icloud.com>
2022-12-14 20:48:23 +0500

    dz6

---
+++ dz6_task41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры N чисел.
+// Посчитайте сколько чисел чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    count++;
+}
+Console.WriteLine($"Числа болше 0: {count}");
---
+++ dz6_task43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2.
+
+
+Console.Clear();
+Console.Write("Введите k и b первой прямой: ");
+string[] nambersFirst = Console.ReadLine().Split(" ");
+double k1 = double.Parse(nambersFirst[0]);
+double b1 = double.Parse(nambersFirst[1]);
+
+Console.Write("Введите k и b второй прямой: ");
+string[] nambersSecond = Console.ReadLine().Split(" ");
+double k2 = double.Parse(nambersSecond[0]);
+double b2 = double.Parse(nambersSecond[1]);
+
+Console.WriteLine((b2 - b1) / (k1 - k2));
+Console.WriteLine(k2 * (b2 - b1) / (k1 - k2) + b2);
