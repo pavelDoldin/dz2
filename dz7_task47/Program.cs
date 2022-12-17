cd500d3ab25088166ddad4117519cfc39dd4c93e
pavelDoldin <doldin86@icloud.com>
2022-12-17 19:33:14 +0500

    dz7

---
+++ dz7_task47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двухмерный массив, размером MxN заполненный случайными 
+// вещественными числами.
+
+void ArrayRandomNumbers(double[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * (10 - 1) + 1;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i, j], 1) + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[m, n];
+ArrayRandomNumbers(numbers);
+PrintArray(numbers);
---
+++ dz7_task50/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая на вход принемает позиции элемента в двухмерном мвссиве
+// и возращает значения этого элемента или же указания, что такого элемента нет.
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[, ] matrix, int x, int y)
+{
+    if (x >= matrix.GetLength(0) || y >= matrix.GetLength(1) || x < 0 || y < 0)
+        Console.WriteLine("К сожалению, Вы ошиблись в координатах");
+    else
+        Console.WriteLine(matrix[x, y]);
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы через пробел: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.Write("Введите позицию элемента ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+ReleaseMatrix(matrix, coord[0] - 1, coord[1] - 1);
